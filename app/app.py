9c7c2c66d8c3597ae2a07d02b76843a624bb72f7
YusukeIkeda <yusuke_aono@yahoo.co.jp>
2021-09-01 00:05:27 +0900

    テンプレートファイルの用意

--- app/app.py
+++ app/app.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+# Flaskをインスタンス化
 app = Flask(__name__)
+
+##ここにDB定義
+
+
+#エラーハンドリング
+@app.errorhandler(404)
+def not_found(error):
+  return '404エラー'
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/upload')
+def upload():
+  return render_template('upload.html')
+
+@app.route('/upload_register', methods=['POST'])
+def upload_register():
+  title = request.form['title']
+  if title:
+    file = request.files['file']
+    file_path = secure_filename(file.filename)
+    file_path = 'static/' + file_path
+    file.save(file_path)
+    return file_path
 
 if __name__ == "__main__":
     app.run(debug=True)
